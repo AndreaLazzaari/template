9c7157b00cac19519884176da5874c742d07e303
Andrea Lazzari <andrea.lazzari8@gmail.com>
2024-01-31 15:38:29 +0100

    first commit

---
+++ resources/js/app.js
@@ -0,0 +1,10 @@
+import './bootstrap';
+import '~resources/scss/app.scss';
+
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
+
+// per far comprendere le img che importo a vite
+import.meta.glob([
+    '../img/**'
+]);
